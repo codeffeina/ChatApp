3f8cd4173a8f3871c3f8907d33efc6a08cf6bc97
Fernando Orozco <forozcomercado@gmail.com>
2022-06-07 09:56:35 -0500

    Added root to serve the login page

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const { Server } = require("socket.io");
 const http = require("http");
 const path = require("path");
+const root = path.join(__dirname, "public");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(root));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(root, "login.html"));
+});
 
 server.listen(3000, () => {
   console.log("Listening on port 3000");
