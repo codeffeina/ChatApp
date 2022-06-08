4c7f01821bbfe3f01dd798bbb723d5ddd15f90b1
Fernando Orozco <forozcomercado@gmail.com>
2022-06-08 11:38:31 -0500

    Prepare project to include socket.io

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const root = path.join(__dirname, "public");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const USERNAMES = new Map();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -17,23 +16,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(root, "login.html"));
 });
 
-app.post("/", (req, res) => {
-  let { username, socketId } = req.body;
-  if (!Array.from(USERNAMES.values()).includes(username)) {
-    USERNAMES.set();
-  }
-  res.redirect("/");
-});
-
-io.on("connection", function (socket) {
-  socket.on("login", function (data) {
-    if (!Array.from(USERNAMES.values()).includes(data.username)) {
-      USERNAMES.set(socket.id, data.username);
-      socket.emit("login");
-    }
-    console.log(USERNAMES);
-  });
-});
+io.on("connection", function (socket) {});
 
 server.listen(3000, () => {
   console.log("Listening on port 3000");
